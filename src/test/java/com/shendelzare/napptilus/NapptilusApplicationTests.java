17ab2549ba3725e3f9bd86e05f9830aba2962113
Sibibu <128801369+Sibibu@users.noreply.github.com>
2023-09-17 12:58:18 +0200

    Initial commit
    Add base application

---
+++ src/test/java/com/shendelzare/napptilus/NapptilusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shendelzare.napptilus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NapptilusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
